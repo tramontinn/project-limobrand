f393e2022c4ca82d9c85b116f22df9db30beeb37
Guilherme Vinicios Tramontin <gvtramontin@gmail.com>
2022-02-14 16:33:57 -0300

    Inicial commit
    This first commit was about my first configuration for the website

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
+
+import logo from '../../assets/logo.jpeg';
+import useStyles from './styles'
+
+const Navbar = ( { totalItems } ) => {
+    const classes = useStyles();
+    const location = useLocation();
+
+    
+
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="LimoBrand" height="90px" className={classes.image} />
+                        Limo Brand
+                    </Typography>
+                    <div className={classes.grow} />
+                    {location.pathname == '/' && (
+                    <div className={classes.button}>
+                        <IconButton component={Link} to="/carrinho" aria-label="Mostar itens do carrinho" color="inherit">
+                            <Badge badgeContent={totalItems} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div> )}
+                </Toolbar>
+            </AppBar>
+        </>
+  )
+};
+
+export default Navbar;
